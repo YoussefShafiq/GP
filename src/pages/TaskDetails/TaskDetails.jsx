8c4f806f89a0c54a20a34d7da3667e545bf625b2
Youssef Shafek <youssefmsa616@gmail.com>
2025-02-25 20:45:13 +0200

    task logs event update
    Signed-off-by: Youssef Shafek <youssefmsa616@gmail.com>

--- src/pages/TaskDetails/TaskDetails.jsx
+++ src/pages/TaskDetails/TaskDetails.jsx
@@ -745,6 +745,7 @@ export default function TaskDetails() {
                                 </> : <>
                                     <ol className="relative border-s border-gray-400 dark:border-gray-700 ">
                                         {taskData?.data?.data?.task.logs.map((log) => (
+
                                             <li key={log.id} className="mb-5 ms-6">
                                                 <span className="absolute flex items-center justify-center w-6 h-6 rounded-full -start-3 dark:bg-blue-900">
                                                     <Tooltip content={log.user.email} delay={350} closeDelay={0}>
@@ -756,10 +757,10 @@ export default function TaskDetails() {
                                                         </div>
                                                     </Tooltip>
                                                 </span>
-                                                <div className="items-center justify-between p-4 bg-white border border-gray-200 rounded-lg shadow-xs sm:flex dark:bg-gray-700 dark:border-gray-600">
+                                                <div className="items-center justify-between p-4 bg-white border border-gray-200 rounded-lg shadow-xs sm:flex  dark:bg-gray-700 dark:border-gray-600">
                                                     <time className="mb-1 text-xs font-normal text-gray-400 sm:order-last sm:mb-0">{formatTimeAgo(log.created_at)}</time>
-
-                                                    <div className="text-sm font-normal text-gray-500 dark:text-gray-300 break-words"><span className='font-bold text-black dark:text-white'>{log.user.name}</span> {log.description}</div>
+                                                    {/* <event className='w-full text-xs font-thin ' ><p className='bg-highlight w-fit text-xs font-thin text-white rounded-lg px-1'>{log.event}</p></event> */}
+                                                    <div className="text-sm font-normal text-gray-500 dark:text-gray-300 break-words"><span className='font-bold text-black dark:text-white'>{log.user.name}</span> {log.description}<div className={`${log.event === 'updated' ? 'bg-yellow-400' : log.event === 'viewed' ? 'bg-blue-500' : 'bg-green-500'} w-fit mt-1 me-1 text-xs font-thin text-white rounded-lg px-1`}>{log.event}</div></div>
                                                 </div>
                                             </li>
                                         ))}
